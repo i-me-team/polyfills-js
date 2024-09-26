38567d32e1da5155c80bbcd322a3541c4ca0376d
mandy8055_fe <msankrit@gmail.com>
2024-09-26 21:39:32 +0530

    [TE-7] :sparkles: Added 2 more polyfills and updated readme

---
+++ src/polyfills/fetchWithTimeout.js
@@ -0,0 +1,37 @@
+// If the server takes more than 2 second to respond; throw and Error as Request timed out
+// fetch(`https://reqres.in/api/users?delay=3`)
+//   .then((resp) => resp.json())
+//   .then(({ data }) =>
+//     data.forEach((datum) =>
+//       console.log(`${datum.first_name} ${datum.last_name}`)
+//     )
+//   );
+
+async function fetchWithTimeout(resource, options = {}) {
+  const { timeout = 10000 } = options;
+
+  const controller = new AbortController();
+  const { signal } = controller;
+  const timerId = setTimeout(() => {
+    controller.abort();
+  }, timeout);
+  const response = await fetch(resource, { ...options, signal });
+  // Cleanup task
+  clearTimeout(timerId);
+  return response;
+}
+
+fetchWithTimeout(`https://reqres.in/api/users?delay=3`, { timeout: 2000 })
+  .then((resp) => resp.json())
+  .then(({ data }) =>
+    data.forEach((datum) =>
+      console.log(`${datum.first_name} ${datum.last_name}`),
+    ),
+  )
+  .catch((error) => {
+    if (error.name === 'AbortError') {
+      console.error('Request timed out');
+    } else {
+      console.error('Some unknown error occured');
+    }
+  });
